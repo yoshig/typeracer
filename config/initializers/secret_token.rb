83009b718f9d49143c74bc27bf53b2c472001d5d
Yoshi Gillaspie <yoshi.gillaspie@gmail.com>
2014-03-18 19:35:30 -0700

    Beginning TypeRacer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TypeRacer::Application.config.secret_key_base = 'ae7b7055937a9774fbb4b86e722234b873fe59a0b02e63754385aa74629c45c98ce22d1cf235c839ba407704ed6f2bf20abd566b245123b363765ab0e771cf30'
